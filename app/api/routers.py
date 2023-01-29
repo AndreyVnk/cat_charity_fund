3687a5f2a526527632e6971756daefd221951bab
Andrei Bychkov <andrey.bychkov.fx@gmail.com>
2023-01-30 00:11:11 +0400

    Add (endpoints, routers and services).google_api

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (charity_project_router, donation_router,
-                               user_router)
+                               google_api_router, user_router)
 
 main_router = APIRouter()
 main_router.include_router(
@@ -11,5 +11,8 @@ main_router.include_router(
 main_router.include_router(
     charity_project_router,
     prefix='/charity_project', tags=['Charity Projects']
+)
+main_router.include_router(
+    google_api_router, prefix='/google', tags=['Google']
 )
 main_router.include_router(user_router)
